0a0bc28b996f2c9b46b06915ac0f83180dc62aa4
Geovanny Mendoza <me@geovannycode.com>
2026-02-07 00:49:08 -0500

    add payments core build

---
+++ payments/core/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation("io.vavr:vavr:0.11.0")
+    
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
