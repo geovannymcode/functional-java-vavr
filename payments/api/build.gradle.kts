0772cbd78978df3e02c65d40930f8517a4d6abf8
Geovanny Mendoza <me@geovannycode.com>
2026-02-07 00:48:44 -0500

    add payments api

---
+++ payments/api/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.springframework.boot") version "4.0.2"
+    id("io.spring.dependency-management") version "1.1.7"
+    java
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Jackson para Option / Either
+    implementation("io.vavr:vavr-jackson:1.0.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ payments/api/src/main/java/com/geovannycode/payments/api/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package com.geovannycode.payments.api.config;
+
+import io.vavr.jackson.datatype.VavrModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public VavrModule vavrModule() {
+        return new VavrModule();
+    }
+}
